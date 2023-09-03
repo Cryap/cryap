ce8cca69d7e038b468568f5d1264075adc99057b
Maxim Leshchenko <maksales@riseup.net>
2023-09-03 15:51:09 +0200

    style: format flake.nix

--- flake.nix
+++ flake.nix
@@ -24,56 +24,67 @@
     };
   };
 
-  outputs = { self, nixpkgs, crane, rust-overlay, flake-utils, advisory-db, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (import rust-overlay)
-          ];
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    rust-overlay,
+    flake-utils,
+    advisory-db,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (import rust-overlay)
+        ];
+      };
 
-	toolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-          extensions = [ "rust-src" ];
-          targets = [ "wasm32-unknown-unknown" ];
+      toolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+        toolchain.default.override {
+          extensions = ["rust-src"];
+          targets = ["wasm32-unknown-unknown"];
         });
 
-        inherit (pkgs) lib;
+      inherit (pkgs) lib;
 
-        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
-        src = craneLib.cleanCargoSource (craneLib.path ./.);
+      craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+      src = craneLib.cleanCargoSource (craneLib.path ./.);
 
-        # Common arguments can be set here to avoid repeating them later
-        commonArgs = {
-          inherit src;
-	  pname = "cryap";
-	  version = "0.0.1";
-          buildInputs = [
-	    pkgs.postgresql
-	    pkgs.yarn
+      # Common arguments can be set here to avoid repeating them later
+      commonArgs = {
+        inherit src;
+        pname = "cryap";
+        version = "0.0.1";
+        buildInputs =
+          [
+            pkgs.postgresql
+            pkgs.yarn
             # Add additional build inputs here
-          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+          ]
+          ++ lib.optionals pkgs.stdenv.isDarwin [
             # Additional darwin specific inputs can be set here
             pkgs.libiconv
           ];
 
-          # Additional environment variables can be set directly
-          # MY_CUSTOM_VAR = "some value";
-        };
+        # Additional environment variables can be set directly
+        # MY_CUSTOM_VAR = "some value";
+      };
 
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+      # Build *just* the cargo dependencies, so we can reuse
+      # all of that work (e.g. via cachix) when running in CI
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
-        my-crate = craneLib.buildPackage (commonArgs // {
+      # Build the actual crate itself, reusing the dependency
+      # artifacts from above.
+      my-crate = craneLib.buildPackage (commonArgs
+        // {
           inherit cargoArtifacts;
         });
-      in
-      {
-        checks = {
+    in {
+      checks =
+        {
           # Build the crate as part of `nix flake check` for convenience
           inherit my-crate;
 
@@ -83,14 +94,16 @@
           # Note that this is done as a separate derivation so that
           # we can block the CI if there are issues here, but not
           # prevent downstream consumers from building our crate by itself.
-          my-crate-clippy = craneLib.cargoClippy (commonArgs // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-          });
+          my-crate-clippy = craneLib.cargoClippy (commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            });
 
-          my-crate-doc = craneLib.cargoDoc (commonArgs // {
-            inherit cargoArtifacts;
-          });
+          my-crate-doc = craneLib.cargoDoc (commonArgs
+            // {
+              inherit cargoArtifacts;
+            });
 
           # Check formatting
           my-crate-fmt = craneLib.cargoFmt {
@@ -105,42 +118,45 @@
           # Run tests with cargo-nextest
           # Consider setting `doCheck = false` on `my-crate` if you do not want
           # the tests to run twice
-          my-crate-nextest = craneLib.cargoNextest (commonArgs // {
-            inherit cargoArtifacts;
-            partitions = 1;
-            partitionType = "count";
-          });
-        } // lib.optionalAttrs (system == "x86_64-linux") {
+          my-crate-nextest = craneLib.cargoNextest (commonArgs
+            // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+            });
+        }
+        // lib.optionalAttrs (system == "x86_64-linux") {
           # NB: cargo-tarpaulin only supports x86_64 systems
           # Check code coverage (note: this will not upload coverage anywhere)
-          my-crate-coverage = craneLib.cargoTarpaulin (commonArgs // {
-            inherit cargoArtifacts;
-          });
+          my-crate-coverage = craneLib.cargoTarpaulin (commonArgs
+            // {
+              inherit cargoArtifacts;
+            });
         };
 
-        packages = {
-          default = my-crate;
-        };
+      packages = {
+        default = my-crate;
+      };
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = my-crate;
-        };
+      apps.default = flake-utils.lib.mkApp {
+        drv = my-crate;
+      };
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = builtins.attrValues self.checks.${system};
+      devShells.default = pkgs.mkShell {
+        inputsFrom = builtins.attrValues self.checks.${system};
 
-	  shellHook = ''
-    export FRONTEND_DIST=$(pwd)/crates/frontend/dist
-  '';
+        shellHook = ''
+          export FRONTEND_DIST=$(pwd)/crates/frontend/dist
+        '';
 
-          # Extra inputs can be added here
-          nativeBuildInputs = with pkgs; [
-	    toolchain
-	    openssl
-	    pkg-config
-	    yarn
-	    trunk
-          ];
-        };
-      });
+        # Extra inputs can be added here
+        nativeBuildInputs = with pkgs; [
+          toolchain
+          openssl
+          pkg-config
+          yarn
+          trunk
+        ];
+      };
+    });
 }
